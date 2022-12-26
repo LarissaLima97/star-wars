743c9b463b6e99d510e368fc842192553f416737
LarissaLima97 <larissa.lmaria@gmail.com>
2022-12-26 18:10:31 -0300

    Adicionando botão na homepage e configurando layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
+import ButtonStep from "../components/ButtonStep";
+import "./Home.css"
+
 const Home = () => {
   return (
     <div> 
         <h1>Personagens Star Wars</h1>
+        <div className="button-previous">
+          <ButtonStep>Anterior</ButtonStep>
+        </div>
+        <div className="button-next">
+          <ButtonStep>Próximo</ButtonStep>
+        </div>
     </div>
+
   );
 };
 
