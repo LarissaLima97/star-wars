b07f0c7c74eda8f12d63329d02128544635616ea
LarissaLima97 <larissa.lmaria@gmail.com>
2022-12-29 20:39:30 -0300

    Iniciando conexão com a API na página de person

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,95 +1,97 @@
 import CardItem from "../components/CardItem";
 import { Grid } from "@mui/material";
 import "./Person.css"
+import { getPerson} from "../services/api";
+import { useState, useEffect } from "react";
 
 
-const character =  {
-  "name": "Luke Skywalker",
-  "height": "172",
-  "mass": "77",
-  "hair_color": "blond",
-  "skin_color": "fair",
-  "eye_color": "blue",
-  "birth_year": "19BBY",
-  "gender": "male",
-  "homeworld": "https://swapi.dev/api/planets/1/",
-  "films": [
-      "https://swapi.dev/api/films/1/",
-      "https://swapi.dev/api/films/2/",
-      "https://swapi.dev/api/films/3/",
-      "https://swapi.dev/api/films/6/"
-  ],
-  "species": [],
-  "vehicles": [
-      "https://swapi.dev/api/vehicles/14/",
-      "https://swapi.dev/api/vehicles/30/"
-  ],
-  "starships": [
-      "https://swapi.dev/api/starships/12/",
-      "https://swapi.dev/api/starships/22/"
-  ],
-  "created": "2014-12-09T13:50:51.644000Z",
-  "edited": "2014-12-20T21:17:56.891000Z",
-  "url": "https://swapi.dev/api/people/1/"
-}
+// const character =  {
+//   "name": "Luke Skywalker",
+//   "height": "172",
+//   "mass": "77",
+//   "hair_color": "blond",
+//   "skin_color": "fair",
+//   "eye_color": "blue",
+//   "birth_year": "19BBY",
+//   "gender": "male",
+//   "homeworld": "https://swapi.dev/api/planets/1/",
+//   "films": [
+//       "https://swapi.dev/api/films/1/",
+//       "https://swapi.dev/api/films/2/",
+//       "https://swapi.dev/api/films/3/",
+//       "https://swapi.dev/api/films/6/"
+//   ],
+//   "species": [],
+//   "vehicles": [
+//       "https://swapi.dev/api/vehicles/14/",
+//       "https://swapi.dev/api/vehicles/30/"
+//   ],
+//   "starships": [
+//       "https://swapi.dev/api/starships/12/",
+//       "https://swapi.dev/api/starships/22/"
+//   ],
+//   "created": "2014-12-09T13:50:51.644000Z",
+//   "edited": "2014-12-20T21:17:56.891000Z",
+//   "url": "https://swapi.dev/api/people/1/"
+// }
 
-const naves = [{
-  "name": "X-wing",
-  "model": "T-65 X-wing",
-  "manufacturer": "Incom Corporation",
-  "cost_in_credits": "149999",
-  "length": "12.5",
-  "max_atmosphering_speed": "1050",
-  "crew": "1",
-  "passengers": "0",
-  "cargo_capacity": "110",
-  "consumables": "1 week",
-  "hyperdrive_rating": "1.0",
-  "MGLT": "100",
-  "starship_class": "Starfighter",
-  "pilots": [
-      "https://swapi.dev/api/people/1/",
-      "https://swapi.dev/api/people/9/",
-      "https://swapi.dev/api/people/18/",
-      "https://swapi.dev/api/people/19/"
-  ],
-  "films": [
-      "https://swapi.dev/api/films/1/",
-      "https://swapi.dev/api/films/2/",
-      "https://swapi.dev/api/films/3/"
-  ],
-  "created": "2014-12-12T11:19:05.340000Z",
-  "edited": "2014-12-20T21:23:49.886000Z",
-  "url": "https://swapi.dev/api/starships/12/"
-},
-{
-  "name": "Imperial shuttle",
-  "model": "Lambda-class T-4a shuttle",
-  "manufacturer": "Sienar Fleet Systems",
-  "cost_in_credits": "240000",
-  "length": "20",
-  "max_atmosphering_speed": "850",
-  "crew": "6",
-  "passengers": "20",
-  "cargo_capacity": "80000",
-  "consumables": "2 months",
-  "hyperdrive_rating": "1.0",
-  "MGLT": "50",
-  "starship_class": "Armed government transport",
-  "pilots": [
-      "https://swapi.dev/api/people/1/",
-      "https://swapi.dev/api/people/13/",
-      "https://swapi.dev/api/people/14/"
-  ],
-  "films": [
-      "https://swapi.dev/api/films/2/",
-      "https://swapi.dev/api/films/3/"
-  ],
-  "created": "2014-12-15T13:04:47.235000Z",
-  "edited": "2014-12-20T21:23:49.900000Z",
-  "url": "https://swapi.dev/api/starships/22/"
-}
-]
+// const naves = [{
+//   "name": "X-wing",
+//   "model": "T-65 X-wing",
+//   "manufacturer": "Incom Corporation",
+//   "cost_in_credits": "149999",
+//   "length": "12.5",
+//   "max_atmosphering_speed": "1050",
+//   "crew": "1",
+//   "passengers": "0",
+//   "cargo_capacity": "110",
+//   "consumables": "1 week",
+//   "hyperdrive_rating": "1.0",
+//   "MGLT": "100",
+//   "starship_class": "Starfighter",
+//   "pilots": [
+//       "https://swapi.dev/api/people/1/",
+//       "https://swapi.dev/api/people/9/",
+//       "https://swapi.dev/api/people/18/",
+//       "https://swapi.dev/api/people/19/"
+//   ],
+//   "films": [
+//       "https://swapi.dev/api/films/1/",
+//       "https://swapi.dev/api/films/2/",
+//       "https://swapi.dev/api/films/3/"
+//   ],
+//   "created": "2014-12-12T11:19:05.340000Z",
+//   "edited": "2014-12-20T21:23:49.886000Z",
+//   "url": "https://swapi.dev/api/starships/12/"
+// },
+// {
+//   "name": "Imperial shuttle",
+//   "model": "Lambda-class T-4a shuttle",
+//   "manufacturer": "Sienar Fleet Systems",
+//   "cost_in_credits": "240000",
+//   "length": "20",
+//   "max_atmosphering_speed": "850",
+//   "crew": "6",
+//   "passengers": "20",
+//   "cargo_capacity": "80000",
+//   "consumables": "2 months",
+//   "hyperdrive_rating": "1.0",
+//   "MGLT": "50",
+//   "starship_class": "Armed government transport",
+//   "pilots": [
+//       "https://swapi.dev/api/people/1/",
+//       "https://swapi.dev/api/people/13/",
+//       "https://swapi.dev/api/people/14/"
+//   ],
+//   "films": [
+//       "https://swapi.dev/api/films/2/",
+//       "https://swapi.dev/api/films/3/"
+//   ],
+//   "created": "2014-12-15T13:04:47.235000Z",
+//   "edited": "2014-12-20T21:23:49.900000Z",
+//   "url": "https://swapi.dev/api/starships/22/"
+// }
+// ]
 
 const convertHeight = (height) => {
   const partialHeight =  height/100;
@@ -103,16 +105,31 @@ const extractId = (url) => {
   return url.replace("https://swapi.dev/api/starships/", "").replace("/", "")
 }
 
+const extractIdFromPath = (url) => {
+  return url.replace("/personagem/","");
+}
 
 
-const Person = () => {
+
+const Person = (args) => {
+  const[character, setCharacter] = useState({});
+  const naves = [];
+  const getPersonInfos = async (id) =>{
+    const res = await getPerson(id)
+    setCharacter(res)
+  };
+
+    useEffect(()=>{
+      getPersonInfos(extractIdFromPath(args.location.pathname))
+    },[])
+
   return (
     <div>
       <h1>{character.name}</h1>
       <section className="character-info">
         <p><b>Data de nascimento:</b>{character.birth_year}</p>
         <p><b>Altura:</b> {convertHeight(character.height)} </p>
-        <p><b>Aparece em:</b> {character.films.length} filmes </p>
+        <p><b>Aparece em:</b> {character.films?.length} filmes </p>
       </section>
       <div class="line">
         <hr></hr>
--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,27 @@ const BASE_URL = "https://swapi.dev/api"
 
 export const getPeopleList = async (page) => {
   const res = await(
-     axios.get(
+    axios.get(
     `${BASE_URL}/people/?page=${page}`
-  ));
+    )
+  );
   return res.data
 };
+
+export const getPerson = async (id) => {
+  const res = await(
+    axios.get(
+      `${BASE_URL}/people/${id}`
+    )
+  );
+  return res.data
+}
+
+export const getStarShip = async (id) => {
+  const res = await(
+    axios.get(
+      `${BASE_URL}/starships/${id}`
+    )
+  );
+  return res.data
+}
