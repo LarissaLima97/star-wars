2f6d35e8479298377039e6d811d0e0a6ccfcd163
LarissaLima97 <larissa.lmaria@gmail.com>
2023-01-01 21:42:45 -0300

    Finalizado conexão com a API

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,127 +1,65 @@
 import CardItem from "../components/CardItem";
 import { Grid } from "@mui/material";
 import "./Person.css"
-import { getPerson} from "../services/api";
+import { getPerson, getStarShip} from "../services/api";
 import { useState, useEffect } from "react";
 
 
-// const character =  {
-//   "name": "Luke Skywalker",
-//   "height": "172",
-//   "mass": "77",
-//   "hair_color": "blond",
-//   "skin_color": "fair",
-//   "eye_color": "blue",
-//   "birth_year": "19BBY",
-//   "gender": "male",
-//   "homeworld": "https://swapi.dev/api/planets/1/",
-//   "films": [
-//       "https://swapi.dev/api/films/1/",
-//       "https://swapi.dev/api/films/2/",
-//       "https://swapi.dev/api/films/3/",
-//       "https://swapi.dev/api/films/6/"
-//   ],
-//   "species": [],
-//   "vehicles": [
-//       "https://swapi.dev/api/vehicles/14/",
-//       "https://swapi.dev/api/vehicles/30/"
-//   ],
-//   "starships": [
-//       "https://swapi.dev/api/starships/12/",
-//       "https://swapi.dev/api/starships/22/"
-//   ],
-//   "created": "2014-12-09T13:50:51.644000Z",
-//   "edited": "2014-12-20T21:17:56.891000Z",
-//   "url": "https://swapi.dev/api/people/1/"
-// }
-
-// const naves = [{
-//   "name": "X-wing",
-//   "model": "T-65 X-wing",
-//   "manufacturer": "Incom Corporation",
-//   "cost_in_credits": "149999",
-//   "length": "12.5",
-//   "max_atmosphering_speed": "1050",
-//   "crew": "1",
-//   "passengers": "0",
-//   "cargo_capacity": "110",
-//   "consumables": "1 week",
-//   "hyperdrive_rating": "1.0",
-//   "MGLT": "100",
-//   "starship_class": "Starfighter",
-//   "pilots": [
-//       "https://swapi.dev/api/people/1/",
-//       "https://swapi.dev/api/people/9/",
-//       "https://swapi.dev/api/people/18/",
-//       "https://swapi.dev/api/people/19/"
-//   ],
-//   "films": [
-//       "https://swapi.dev/api/films/1/",
-//       "https://swapi.dev/api/films/2/",
-//       "https://swapi.dev/api/films/3/"
-//   ],
-//   "created": "2014-12-12T11:19:05.340000Z",
-//   "edited": "2014-12-20T21:23:49.886000Z",
-//   "url": "https://swapi.dev/api/starships/12/"
-// },
-// {
-//   "name": "Imperial shuttle",
-//   "model": "Lambda-class T-4a shuttle",
-//   "manufacturer": "Sienar Fleet Systems",
-//   "cost_in_credits": "240000",
-//   "length": "20",
-//   "max_atmosphering_speed": "850",
-//   "crew": "6",
-//   "passengers": "20",
-//   "cargo_capacity": "80000",
-//   "consumables": "2 months",
-//   "hyperdrive_rating": "1.0",
-//   "MGLT": "50",
-//   "starship_class": "Armed government transport",
-//   "pilots": [
-//       "https://swapi.dev/api/people/1/",
-//       "https://swapi.dev/api/people/13/",
-//       "https://swapi.dev/api/people/14/"
-//   ],
-//   "films": [
-//       "https://swapi.dev/api/films/2/",
-//       "https://swapi.dev/api/films/3/"
-//   ],
-//   "created": "2014-12-15T13:04:47.235000Z",
-//   "edited": "2014-12-20T21:23:49.900000Z",
-//   "url": "https://swapi.dev/api/starships/22/"
-// }
-// ]
-
 const convertHeight = (height) => {
   const partialHeight =  height/100;
-  const stringHeight = partialHeight.toString()
-  const splitedHeight = stringHeight.split(".")
+  const stringHeight = partialHeight.toString();
+  const splitedHeight = stringHeight.split(".");
   
-  return `${splitedHeight[0]} metro(s) e ${splitedHeight[1]} centímetros`
+  return `${splitedHeight[0]} metro(s) e ${splitedHeight[1]} centímetros`;
 }
 
 const extractId = (url) => {
-  return url.replace("https://swapi.dev/api/starships/", "").replace("/", "")
-}
+  return url.replace("https://swapi.dev/api/starships/", "").replace("/", "");
+};
 
 const extractIdFromPath = (url) => {
   return url.replace("/personagem/","");
-}
-
-
+};
 
 const Person = (args) => {
   const[character, setCharacter] = useState({});
-  const naves = [];
+  const[starShipList, setStarShipList] = useState([]);
+  let starShips = [];
+  let starShipIds = [];
+
   const getPersonInfos = async (id) =>{
-    const res = await getPerson(id)
-    setCharacter(res)
+    const res = await getPerson(id);
+    setCharacter(res);
   };
 
+  const populateStarshipIds = () => {
+    for (let i=0; i < character.starships?.length; i++){
+      let id = extractId(character.starships[i]);
+      starShipIds.push(id);
+    }
+    populateStarShipList();
+  };
+  
+  const getStarShipInfo = async (id) => {
+    const response = await getStarShip(id);
+    return response;
+  }
+
+  const populateStarShipList = async () => {
+    for (let i=0; i < starShipIds.length; i++){
+      const starShip = await getStarShipInfo(starShipIds[i]);
+      starShips.push(starShip);
+    }
+    setStarShipList(starShips);
+  };
+    
     useEffect(()=>{
-      getPersonInfos(extractIdFromPath(args.location.pathname))
-    },[])
+      getPersonInfos(extractIdFromPath(args.location.pathname));
+    },[args.location.pathname]);
+
+    useEffect(()=>{
+      populateStarshipIds()
+    },[character]);
 
   return (
     <div>
@@ -131,13 +69,13 @@ const Person = (args) => {
         <p><b>Altura:</b> {convertHeight(character.height)} </p>
         <p><b>Aparece em:</b> {character.films?.length} filmes </p>
       </section>
-      <div class="line">
+      <div className="line">
         <hr></hr>
       </div>
       <section className="starship">
         <h2>Naves</h2>
         <Grid container spacing={2} >
-            {naves.map(starship =>(
+            {starShipList.map(starship =>(
               <Grid item  xs={12} key={extractId(starship.url)}>
                 <CardItem to={`/nave/${extractId(starship.url)}`} >{starship.name}</CardItem>
               </Grid>
